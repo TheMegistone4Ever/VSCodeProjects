3fb5c4f651834dd34263fadef81839272fd8e532
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-10-24 19:33:28 +0300

    do almost 5 lab

--- WEB_2_5_LAB_GRID/scripts/script.js
+++ WEB_2_5_LAB_GRID/scripts/script.js
@@ -1,4 +1,4 @@
-var swapclicks = 0;
+var swapclicks = 1;
 
 function swapText(id1, id2) {
     let elem1 = document.getElementById(id1);
@@ -15,7 +15,7 @@ function swapText(id1, id2) {
         if (el.tagName != "H3") elem2.append(el);
     });
 
-    if (swapclicks++ % 2 == 0) {
+    if (++swapclicks % 2 == 0) {
         elem1.style.gridTemplateColumns = "1fr 1fr 1fr 2fr";
         elem2.style.gridTemplateColumns = "1fr 2fr";
     } else {
@@ -39,6 +39,7 @@ function getRhombusArea(outId, inId1, inId2) {
             document.getElementById(inId2).style.color = "#f00";
             alert("Друга діагональ повинна бути додатною!");
         } else {
+            document.getElementById(inId1).style.color = "#000";
             elem = document.createElement("H3");
             elem.innerHTML = "Площа ромбу (d1 = " + elem1.value + ", d2 = " + elem2.value + "): " + (elem1.value * elem2.value / 2);
             document.getElementById(outId).append(elem);
@@ -46,3 +47,33 @@ function getRhombusArea(outId, inId1, inId2) {
     }
 }
 
+window.addEventListener('load', (event) => {
+    if (document.cookie != "max-element=; min-element=") {
+        if (confirm("cookies: " + document.cookie + "\nClear cookies?")) {
+            document.cookie = "max-element=;"
+            document.cookie = "min-element=;";
+            location.reload();
+        } else {
+            alert("Cookies haven't been deleted, please reload the web page");
+        }
+    }
+});
+
+function minmax(classname) {
+    let elements = document.getElementsByClassName(classname);
+    let min = elements[0].value;
+    let max = elements[0].value;
+    for (let el = 1; el < elements.length; ++el) {
+        if (elements[el].value < min) min = elements[el].value;
+        else if (elements[el].value > max) max = elements[el].value;
+    }
+
+    document.cookie = "min-element= " + min + ";";
+    document.cookie = "max-element= " + max + ";";
+
+    alert("min-element=" + min + ";\nmax element=" + max + ";");
+}
+
+function loadColorInLocalStorage() {
+
+}
