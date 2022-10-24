b4ee8e62a21b7744a3fb5b6042284dce1e3c4aaf
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-10-24 12:30:41 +0300

    do 2 funcs

---
+++ WEB_2_5_LAB_GRID/scripts/script.js
@@ -0,0 +1,48 @@
+var swapclicks = 0;
+
+function swapText(id1, id2) {
+    let elem1 = document.getElementById(id1);
+    let elem2 = document.getElementById(id2);
+
+    let arr1 = Array.from(elem1.children);
+    let arr2 = Array.from(elem2.children);
+
+    arr2.forEach(el => {
+        if (el.tagName != "h3") elem1.append(el);
+    });
+
+    arr1.forEach(el => {
+        if (el.tagName != "h3") elem2.append(el);
+    });
+
+    if (swapclicks++ % 2 == 0) {
+        elem1.style.gridTemplateColumns = "1fr 1fr 1fr 2fr";
+        elem2.style.gridTemplateColumns = "1fr 2fr";
+    } else {
+        elem2.style.gridTemplateColumns = "2fr 1fr 1fr 1fr";
+        elem1.style.gridTemplateColumns = "2fr 1fr";
+    }
+
+    elem1.append(arr1.pop());
+}
+
+function getRhombusArea(outId, inId1, inId2) {
+    elem1 = document.getElementById(inId1);
+    elem2 = document.getElementById(inId2);
+
+    if (elem1.value <= 0) {
+        document.getElementById(inId1).style.color = "#f00";
+        alert("Перша діагональ повинна бути додатною!");
+    } else {
+        document.getElementById(inId1).style.color = "#000";
+        if (elem2.value <= 0) {
+            document.getElementById(inId2).style.color = "#f00";
+            alert("Друга діагональ повинна бути додатною!");
+        } else {
+            elem = document.createElement("h3");
+            elem.innerHTML = "Площа ромбу (d1 = " + elem1.value + ", d2 = " + elem2.value + "): " + (elem1.value * elem2.value / 2);
+            document.getElementById(outId).append(elem);
+        }
+    }
+}
+
