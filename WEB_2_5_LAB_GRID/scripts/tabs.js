100c1ea66d14f417808631a9d0d12984d60c9956
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-12-26 22:36:55 +0200

    Do 5 lab

---
+++ WEB_2_5_LAB_GRID/scripts/tabs.js
@@ -0,0 +1,30 @@
+let tabCountName = "countOfTabs", tabs = Number(localStorage.getItem(tabCountName)), limitOfTabs = 6, tabNameInLS = "tab";
+
+function addTab() {
+    if (++tabs >= limitOfTabs) return;
+    elem = document.createElement("LI");
+    elem.innerHTML = '<input type="radio" name="tabs" id="tab'
+        + tabs + '" checked /> <label for="tab'
+        + tabs + '" role="tab" aria-selected="true" aria-controls="panel'
+        + tabs + '" tabindex="0">Tab</label> <div id="tab-content'
+        + tabs + '" class="tab-content" role="tabpanel" aria-labelledby="description" aria-hidden="false"> <p>Content of tab №'
+        + tabs + '</p> </div>';
+    document.getElementById("tabs").appendChild(elem);
+    localStorage.setItem(tabNameInLS + tabs, elem.innerHTML);
+    localStorage.setItem(tabCountName, Number(localStorage.getItem(tabCountName)) + 1);
+}
+
+function readLineFromFile() {
+    return;
+}
+
+window.addEventListener('load', (event) => {
+    list = document.getElementById("tabs");
+    for (let key of Object.keys(localStorage).reverse()) {
+        if (key.startsWith(tabNameInLS)) {
+            elem = document.createElement("LI");
+            elem.innerHTML = localStorage.getItem(key);
+            list.appendChild(elem);
+        }
+    }
+});
